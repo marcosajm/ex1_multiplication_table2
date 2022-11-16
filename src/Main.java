a7daca48d1c9a8edced1646f459ab782dbd6ebbc
marcosajm <52014409+marcosajm@users.noreply.github.com>
2022-11-16 12:01:33 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Tabuada de multiplicação. Escreva o número de 1 a 10:");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.println("Tabuada de multiplicação. Escreva o número de inicial:");
+        Scanner in = new Scanner(System.in);
+        int inicio = in.nextInt();
+        System.out.println("Tabuada de multiplicação. Escreva o número final:");
+        Scanner fm = new Scanner(System.in);
+        int fim = fm.nextInt();
+        for(int i = inicio; i <= fim; ++i) {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
     }
 }
